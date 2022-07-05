0b0a647fd354959f3ceb80a9f04b4313cd4bb1fe
Oladimeji <oluyedis@gmail.com>
2022-07-05 13:23:51 +0100

    main component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Main = () => {
-	return <div>Main</div>;
-};
-
-export default Main;
+export default function Main() {
+	return (
+		<main>
+			<form className="form">
+				<input type="text" placeholder="Top text" className="form-input" />
+				<input type="text" placeholder="Bottom text" className="form-input" />
+				<button className="form-button">Get a new meme image 🖼</button>
+			</form>
+		</main>
+	);
+}
