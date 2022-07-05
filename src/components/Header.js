5729c869d403b3d6e28794e8cdae979e6c6563d9
Oladimeji <oluyedis@gmail.com>
2022-07-05 12:52:41 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+	return (
+		<header className="header">
+			<i className="fa-solid fa-images header-image"></i>
+			<h3 className="header-title">Meme Generator</h3>
+			<h3 className="header-text">Best meme generator</h3>
+		</header>
+	);
+}
